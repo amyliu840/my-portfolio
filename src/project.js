3ee1a59af95339a4b10f7accec8c32dda45a9d33
Amy Liu <liuamy840@hotmail.com>
2019-12-04 21:30:57 +0800

    style

--- src/project.js
+++ src/project.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
+import './project.css';
 
 function Project() {
   const description = 'Car Dealer App';
 
   return (
-    <div id="experience" className="project-container">
+    <div id="project" className="project-container">
       <h3>Projects</h3>
         <Card>
           <Card.Content>
